72d5bfb227f7fcc592cb8743a111f2bc5537acad
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-11 21:30:06 +0200

    Updated Book.java package

---
+++ src/model/Book.java
@@ -0,0 +1,64 @@
+package src.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private Integer isbn;
+    private Integer yearPublished;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, Integer yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public Book(String title, String author, Integer isbn, Integer yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(Integer yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTitle: " + this.title +
+                "\nAuthor: " + this.author +
+                "\nISBN: " + this.isbn +
+                "\nYear: " + this.yearPublished;
+    }
+}
